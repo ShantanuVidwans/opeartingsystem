3492ff2383a82c30e6c0dbba0a1102cbc247ea41
shantanuvidwans <shantanuvidwans123@gmail.com>
2023-10-21 23:27:16 -0400

    Created Benchmark 3

---
+++ A0/benchmarks/benchmark3.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include "../mypthread.h"
+
+/* Global variables */
+int thread_num;
+pthread_t *thread;
+
+/* A CPU-bound task tzo do vector multiplication */
+void call_api(void* arg) {
+	dummyApi();
+	pthread_exit(NULL);
+}
+
+// A dummy function to simulate a long running api with unknown IO times
+void dummyApi(){
+	int r = rand() % 10;
+	for(int i=0; i < 1000000*r; i++) continue;
+}
+
+void verify(int num){
+	struct timespec start, end;
+	clock_gettime(CLOCK_REALTIME, &start);
+
+	for (int i = 0; i < num; i++)
+		call_api(1);
+
+	clock_gettime(CLOCK_REALTIME, &end);
+        printf("Sequential running time: %lu micro-seconds\n", 
+	       (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000);
+}
+
+int main(int argc, char **argv) {
+	int num_api_calls;
+
+	if (argv[1] < 1) {
+			printf("enter a valid thread number\n");
+			return 0;
+	} else {
+			num_api_calls = atoi(argv[1]);
+	}
+	// initialize pthread_t
+	thread = (pthread_t*)malloc(num_api_calls*sizeof(pthread_t));
+
+	struct timespec start, end;
+        clock_gettime(CLOCK_REALTIME, &start);
+
+	for (int i = 0; i < num_api_calls; i++)
+		pthread_create(&thread[i], NULL, &call_api, NULL);
+
+	for (int i = 0; i < num_api_calls; i++)
+		pthread_join(thread[i], NULL);
+
+	clock_gettime(CLOCK_REALTIME, &end);
+        printf("Pthreads running time: %lu micro-seconds\n", 
+	       (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000);
+	verify(num_api_calls);
+
+	// Free memory on Heap
+	free(thread);
+	printf("\n7");
+	return 0;
+}
